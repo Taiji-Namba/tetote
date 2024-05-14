b8bcb99d343eca82cd3f6ab78ca48660cf3a32db
Taiji-Namba <namba9499@gmail.com>
2024-05-14 21:11:12 +0900

    gulpfile.jsを最小限に編集

--- app/public/wp-content/themes/tetote/gulpfile.js
+++ app/public/wp-content/themes/tetote/gulpfile.js
@@ -28,80 +28,12 @@ const compileFrontPageSass = () => {
     .pipe(dest("css", { sourcemaps: "." }));
 };
 
-// sass/page/entry.scssをコンパイル
-const compileEntrySass = () => {
-  return src("sass/page/entry.scss", { sourcemaps: true })
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(postcss([autoprefixer({ grid: true })]))
-    .pipe(dest("./entry/css", { sourcemaps: "." }));
-};
-
-// sass/page/engineer.scssをコンパイル
-const compileEngineerSass = () => {
-  return src("sass/page/engineer.scss", { sourcemaps: true })
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(postcss([autoprefixer({ grid: true })]))
-    .pipe(dest("./engineer/css", { sourcemaps: "." }));
-};
-
-// sass/page/sales.scssをコンパイル
-const compileSalesSass = () => {
-  return src("sass/page/sales.scss", { sourcemaps: true })
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(postcss([autoprefixer({ grid: true })]))
-    .pipe(dest("./sales/css", { sourcemaps: "." }));
-};
-
-// sass/page/data-scientist.scssをコンパイル
-const compileDataScientistSass = () => {
-  return src("sass/page/data-scientist.scss", { sourcemaps: true })
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(postcss([autoprefixer({ grid: true })]))
-    .pipe(dest("./data-scientist/css", { sourcemaps: "." }));
-};
-
-// sass/page/agent.scssをコンパイル
-const compileAgentSass = () => {
-  return src("sass/page/agent.scss", { sourcemaps: true })
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(postcss([autoprefixer({ grid: true })]))
-    .pipe(dest("./agent/css", { sourcemaps: "." }));
-};
-
 // Sassファイルを監視
 const watchSassFiles = () => {
   watch(
     "sass/**/*.scss",
     { ignoreInitial: false }, // パーシャルファイルの変更も検知するためのオプション
-    parallel(
-      compileStyleSass,
-      compileFrontPageSass,
-      compileEntrySass,
-      compileEngineerSass,
-      compileSalesSass,
-      compileDataScientistSass,
-      compileAgentSass
-    )
+    parallel(compileStyleSass, compileFrontPageSass)
   );
 };
 
