d7111b9c64e5c1d179f524668a535274560841de
Taiji-Namba <namba9499@gmail.com>
2024-05-15 15:36:16 +0900

    トップページ用ヘッダーのHTMLコーディング(バーガーメニュー内はまだ)

---
+++ app/public/wp-content/themes/tetote/template-parts/header/header.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head prefix="og: https://ogp.me/ns#">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>採用特設サイト | 株式会社TETOTE</title>
+    <meta
+      name="description"
+      content="テクノロジーで社会課題を解決する。AIやビッグデータ分析などの技術を活用した社会課題解決サービスを提供するTETOTEの採用サイト。"
+    />
+
+    <meta
+      property="og:url"
+      content="https://the-connect-web.com/tetote/"
+    />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:title"
+      content="採用特設サイト | 株式会社TETOTE"
+    />
+    <meta
+      property="og:description"
+      content="テクノロジーで社会課題を解決する。AIやビッグデータ分析などの技術を活用した社会課題解決サービスを提供するTETOTEの採用サイト。"
+    />
+    <meta
+      property="og:site_name"
+      content="採用特設サイト | 株式会社TETOTE"
+    />
+    <meta
+      property="og:image"
+      content="https://the-connect-web.com/tetote/img/ogp-image.jpg"
+    />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta
+      property="twitter:url"
+      content="https://the-connect-web.com/tetote/"
+    />
+    <meta
+      property="twitter:title"
+      content="採用特設サイト | 株式会社TETOTE"
+    />
+    <meta
+      property="twitter:description"
+      content="テクノロジーで社会課題を解決する。AIやビッグデータ分析などの技術を活用した社会課題解決サービスを提供するTETOTEの採用サイト。"
+    />
+    <meta
+      property="twitter:image"
+      content="https://the-connect-web.com/tetote/img/ogp-image.jpg"
+    />
+
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/tetote/icons/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/tetote/icons/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/tetote/icons/favicon-16x16.png"
+    />
+    <link
+      rel="mask-icon"
+      href="/tetote/icons/safari-pinned-tab.svg"
+      color="#5bbad5"
+    />
+    <link rel="shortcut icon" href="/tetote/icons/favicon.ico" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+    <meta name="theme-color" content="#ffffff" />
+
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/destyle.css@4.0.1/destyle.min.css"
+    />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/front-page.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <!-- 全ページで使用するフォント -->
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Viga&display=swap" rel="stylesheet">
+    <!-- トップページ限定で使用するフォント -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Inter:wght@700&display=swap" rel="stylesheet">
+
+    <script
+      src="https://code.jquery.com/jquery-3.7.1.min.js"
+      integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+      crossorigin="anonymous"
+    ></script>
+
+    <meta name="robots" content="noindex" />
+  </head>
+
+  <body class="top-page">
+    <header class="header top-page-header">
+      <div class="header__inner">
+        <a href="#" class="logo header__logo"><img src="./images/logo-black.svg" alt="「TETOTE」のロゴ" class="logo-img"></a>
+        <nav class="gnav">
+          <div class="gnav__inner">
+            <ul class="gnav__list">
+              <li class="gnav__item">
+                <a href="./details/" class="gnav__anchor button button--black"
+                  >募集要項</a
+                >
+              </li>
+              <li class="gnav__item">
+                <a href="./entry/" class="gnav__anchor button button--gold"
+                  >ENTRY</a
+                >
+              </li>
+              <li class="gnav__item">
+                <button
+          type="button"
+          class="burger-button"
+          id="burger-button"
+          aria-controls="burger-nav"
+          aria-expanded="false"
+          aria-label="メニューを開く"
+        ><span class="burger-button__line"></span><span class="burger-button__line"></span><span class="burger-button__line"></span><span class="burger-menu__label" id="burger-menu-label" aria-hidden="true">MENU</span></button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+      </div>
+    </header>
