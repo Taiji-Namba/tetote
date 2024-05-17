019d29888869e83fd4054baedc52779e29fe7237
Taiji-Namba <namba9499@gmail.com>
2024-05-17 09:32:40 +0900

    TOPのFVのHTML

--- app/public/wp-content/themes/tetote/front-page.php
+++ app/public/wp-content/themes/tetote/front-page.php
@@ -1,7 +1,25 @@
 <?php get_header(); ?>
 
 <main class="main">
-  <div class="main__inner"><p>トップページ</p></div>
+  <div class="main__inner">
+    <div class="fv">
+      <div class="fv__slide-container fv-slide">
+        <div class="fv-slide__picture">
+          <div class="semi-transparent"></div>
+        </div>
+        <div class="fv-slide__picture">
+          <div class="semi-transparent"></div>
+        </div>
+      </div>
+      <h2 class="fv__heading">革新で繋げ<br />人と街</h2>
+      <p class="fv__text">君の挑戦が、意思が、未来を変える</p>
+      <div class="news fv__news">
+        <h3 class="news__heading">NEWS</h3>
+        <p class="news__title">ここは採用ブログの最新記事のタイトル</p>
+        <a href="./blog/" class="news_cta">VIEW MORE</a>
+      </div>
+    </div>
+  </div>
 </main>
 
 <?php get_footer(); ?>
