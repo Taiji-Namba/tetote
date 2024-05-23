c5d558ad19e622ff55ac74d467d3c45768d44639
Taiji-Namba <namba9499@gmail.com>
2024-05-23 17:30:39 +0900

    フッターとCTAのPHPコーディング

--- app/public/wp-content/themes/tetote/footer.php
+++ app/public/wp-content/themes/tetote/footer.php
@@ -1,6 +1,36 @@
-    <footer class="footer">
-        <div class="footer__inner">
+    <div class="cta">
+      <div class="cta__image"></div>
+        <div class="cta__container">
+          <p class="cta__text">わたしたちと一緒に働く仲間を募集中です。少数精鋭のチームで、あなたも会社も一緒に成長していきましょう。</p>
+          <a class="button cta__button button--gold" href="">ENTRY</a>
         </div>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <div class="footer__inner">
+        <div class="footer__main-menu">
+          <a href="" class="footer__logo"><img class="logo__img logo__img--black" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/logo-black.svg'); ?>" alt="「TETOTE」のロゴ"></a>
+          <ul class="footer__list">
+            <li class="footer__item">ホーム</li>
+            <li class="footer__item">TETOTEについて</li>
+            <li class="footer__item">人を知る</li>
+            <li class="footer__item">研修制度とキャリアパス</li>
+            <li class="footer__item">福利厚生</li>
+            <li class="footer__item">採用ブログ</li>
+            <li class="footer__item">募集要項</li>
+            <li class="footer__item">よくある質問</li>
+            <li class="footer__item">会社概要</li>
+          </ul>
+        </div>
+        <div class="footer__sub-menu"></div>
+        <p class="footer__copy-right">© 2024 TETOTE All Right Reserved.</p>
+        <ul class="sns-icons">
+          <li class="sns-icons__item"><a href="" class="sns-icons__anchor"><img class="sns-icons__img" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/facebook.svg'); ?>" alt="Facebookのリンク"></a></li>
+          <li class="sns-icons__item"><a href="" class="sns-icons__anchor"><img class="sns-icons__img" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/twitter.svg'); ?>" alt="Twitterのリンク"></a></li>
+          <li class="sns-icons__item"><a href="" class="sns-icons__anchor"><img class="sns-icons__img" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/youtube.svg'); ?>" alt="YouTubeのリンク"></a></li>
+        </ul>
+      </div>
     </footer>
 
     <?php wp_footer(); ?>
