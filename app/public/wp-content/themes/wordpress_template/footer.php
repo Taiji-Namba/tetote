10f0ff54fc866243b63d7830d55df9d135a6f12d
Taiji-Namba <namba9499@gmail.com>
2024-06-04 17:37:56 +0900

    フッターから不要なコメントアウトを削除

--- app/public/wp-content/themes/wordpress_template/footer.php
+++ app/public/wp-content/themes/wordpress_template/footer.php
@@ -33,16 +33,6 @@
   </div>
 </footer>
 
-<!-- 以下コメントアウト -->
-<!-- <div class="p-footer__inner l-inner"> -->
-<!-- </div> -->
-<!-- <div class="p-footer__copyright"> -->
-<!-- <small>
-    Copyright(c)&nbsp;2023&nbsp;All&nbsp;Rights&nbsp;Reserved.
-    </small> -->
-<!-- </div> -->
-<!-- </footer> -->
-
 <?php wp_footer(); ?>
 </div>
 
