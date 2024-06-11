9f4c367a2466e998c2c079dd52e63879c04e3070
Taiji-Namba <namba9499@gmail.com>
2024-06-11 14:32:06 +0900

    MEMBERセクションのスライドのphp

--- app/public/wp-content/themes/tetote/parts/common/p-splide-staff.php
+++ app/public/wp-content/themes/tetote/parts/common/p-splide-staff.php
@@ -1,5 +1,5 @@
 <!-- スライダーのコンテナを作成 -->
-<div id="<?php echo esc_attr($args['post_type']); ?>" class="splide">
+<div id="<?php echo esc_attr($args['post_type']); ?>" class="splide p-staff">
   <div class="splide__track">
     <div class="splide__list">
       <?php
@@ -9,16 +9,18 @@
         while ($the_query->have_posts()) : $the_query->the_post(); // 投稿ループの開始
           // 各フィールドの情報を取得
           if (has_post_thumbnail()) : ?>
-            <div class="splide__slide p-staff">
-              <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail(); ?>
-                <p class="p-staff__message-1"><?php the_field('staff_message_1'); ?></p>
-                <p class="p-staff__message-2"><?php the_field('staff_message_2'); ?></p>
-                <p class="p-staff__brief-introduction">
-                  <span class="p-staff__occupation"><?php the_field('occupation'); ?></span>
-                  <span class="p-staff__year-of-entry"><?php the_field('year_of_entry'); ?>入社</span>
-                </p>
-                <h5 class="p-staff__name"><?php the_field('staff_name'); ?></h5>
+            <div class="splide__slide">
+              <a href="<?php the_permalink(); ?>" class="p-staff__card">
+                <?php the_post_thumbnail('full', ['class' => 'p-staff__thumbnail']); ?>
+                <div class="p-staff__texts">
+                  <p class="p-staff__message-1"><?php the_field('staff_message_1'); ?></p>
+                  <p class="p-staff__message-2"><?php the_field('staff_message_2'); ?></p>
+                  <p class="p-staff__brief-introduction">
+                    <span class="p-staff__occupation"><?php the_field('occupation'); ?></span>
+                    <span class="p-staff__year-of-entry"><?php the_field('year_of_entry'); ?>入社</span>
+                  </p>
+                  <h5 class="p-staff__name"><?php the_field('staff_name'); ?></h5>
+                </div>
               </a>
             </div>
       <?php
