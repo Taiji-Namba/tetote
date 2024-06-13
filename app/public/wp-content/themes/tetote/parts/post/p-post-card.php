f42ca89550331ff7c451ac7ed649d882e578b2d8
Taiji-Namba <namba9499@gmail.com>
2024-06-13 14:09:35 +0900

    BLOG一覧で表示するカードの中身コーディング

--- app/public/wp-content/themes/tetote/parts/post/p-post-card.php
+++ app/public/wp-content/themes/tetote/parts/post/p-post-card.php
@@ -1,4 +1,3 @@
-<!-- カード -->
 <li class="p-post-card">
   <a href="<?php the_permalink(); ?>" class="p-post-card__link">
     <?php
@@ -12,35 +11,32 @@
     }
     ?>
     <div class="p-post-card__body">
-      <!-- 投稿が7日以内であるか判定 -->
-      <?php if (new_posting()) : ?>
-        <div class="p-post-card__new">NEW</div>
-      <?php endif; ?>
       <figure class="p-post-card__img">
         <img src="<?php echo $image_url; ?>" alt="<?php echo $thumbAlt; ?>" loading="lazy">
       </figure>
       <div class="p-post-card__info">
         <?php
         $categories = get_the_category();
-        // カスタム投稿の場合 タクソノミー名を指定する必要あり
-        // $categories = get_the_terms(get_the_ID(), 'area');
+        // カテゴリーが複数登録されている場合は1つめを表示するように
         if (!empty($categories)) :
         ?>
-          <ul class="p-post-card__category">
-            <li><?php echo esc_html($categories[0]->name); ?></li>
-          </ul>
-        <?php
-        endif;
-        ?>
+          <div class="p-post-card__category">
+            <img class="p-post-card__category-img" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/category.svg'); ?>" alt="投稿カテゴリーアイコン">
+            <p class="p-post-card__category-name"><?php echo esc_html($categories[0]->name); ?></p>
+          </div>
+        <?php endif; ?>
+        <h5 class="p-post-card__title">
+          <?php
+          $max_length = 76;
+          omitPostTitle(get_post(), $max_length);
+          ?>
+        </h5>
         <div class="p-post-card__date">
           <!-- 投稿日 -->
           <time datetime="<?php the_time('Y-m-d'); ?>">
             <?php the_time('Y.m.d'); ?>
           </time>
         </div>
-      </div>
-      <div class="p-post-card__title">
-        <?php the_title(); ?>
       </div>
     </div>
   </a>
