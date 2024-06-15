1e5f5b1272997dd5d4438c13104253d42b909a90
Taiji-Namba <namba9499@gmail.com>
2024-06-15 22:30:17 +0900

    mainの下余白の設定

--- app/public/wp-content/themes/tetote/parts/post/p-post-list-mainloop.php
+++ app/public/wp-content/themes/tetote/parts/post/p-post-list-mainloop.php
@@ -3,8 +3,8 @@ $post_type = get_query_var('post_type') ? get_query_var('post_type') : 'post';
 $post_type_data = get_post_type_object($post_type);
 $post_type_label = $post_type_data->labels->name;
 ?>
-<main class="p-archive">
-  <section class="p-post-list p-archive__inner">
+<main class="p-archive p-main">
+  <section class="p-post-list p-archive__inner p-main__inner">
     <div class="p-post-list__wrapper">
       <div class="p-post-list__inner l-inner">
         <ul class="p-post-list__cards">
