6e9c804f0d78caecbf3c19cef818328a53bd582c
Taiji-Namba <namba9499@gmail.com>
2024-06-17 18:48:43 +0900

    大枠としての固定ページ用テンプレートの設置

---
+++ app/public/wp-content/themes/tetote/page-benefits.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: BENEFITS
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
---
+++ app/public/wp-content/themes/tetote/page-details.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: DETAILS
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
