8003d0f72e9aec6840013c5f43c5c8bce197db02
Taiji-Namba <namba9499@gmail.com>
2024-06-19 16:34:56 +0900

    アイキャッチテンプレートパーツをそれぞれのテンプレートファイルで読み込み

--- app/public/wp-content/themes/tetote/page-about-us.php
+++ app/public/wp-content/themes/tetote/page-about-us.php
@@ -6,6 +6,8 @@ Template Name: ABOUT US
 
 <?php get_header(); ?>
 
+<?php get_template_part('parts/common/p-eyecatch'); ?>
+
 <?php get_template_part('parts/common/p-breadcrumb'); ?>
 
 <?php get_footer(); ?>
--- app/public/wp-content/themes/tetote/page-career.php
+++ app/public/wp-content/themes/tetote/page-career.php
@@ -6,6 +6,8 @@ Template Name: CAREER
 
 <?php get_header(); ?>
 
+<?php get_template_part('parts/common/p-eyecatch'); ?>
+
 <?php get_template_part('parts/common/p-breadcrumb'); ?>
 
 <?php get_footer(); ?>
