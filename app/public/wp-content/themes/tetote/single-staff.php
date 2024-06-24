e23dae4806fbad31a42c3080251da6e37eecd4ba
Taiji-Namba <namba9499@gmail.com>
2024-06-24 16:17:47 +0900

    スタッフ詳細ページの枠組みと、投稿本文、サイドバー目次のphp

--- app/public/wp-content/themes/tetote/single-staff.php
+++ app/public/wp-content/themes/tetote/single-staff.php
@@ -1,5 +1,25 @@
 <?php get_header(); ?>
+<main>
+  <article class="<?php post_class('p-article p-staff-article'); ?>">
+    <?php while (have_posts()) : the_post(); ?>
 
-<?php get_template_part('parts/common/p-breadcrumb'); ?>
+      <?php get_template_part('parts/project/p-staff-fv'); ?>
 
+      <?php get_template_part('parts/common/p-breadcrumb'); ?>
+
+      <section class="p-article__contents p-staff-article__contents l-staff">
+        <div class="p-staff-article__contents-inner">
+          <div class="p-staff-article__main-contents">
+            <?php the_content(); ?>
+          </div>
+          <div class="p-staff-article__index">
+            <?php if (is_active_sidebar('sidebar')) : ?>
+              <?php dynamic_sidebar('sidebar'); ?>
+            <?php endif; ?>
+          </div>
+        </div>
+      </section>
+    <?php endwhile; ?>
+  </article>
+</main>
 <?php get_footer(); ?>
