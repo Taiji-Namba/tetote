1e13c1bfaaea8abb4d02d4af81b19634da37fd74
Taiji-Namba <namba9499@gmail.com>
2024-06-24 18:02:46 +0900

    スタッフリストのテンプレートファイルから余分なコードを削除&スタッフ一覧ページにその分を追加

--- app/public/wp-content/themes/tetote/parts/project/p-staff-list.php
+++ app/public/wp-content/themes/tetote/parts/project/p-staff-list.php
@@ -1,15 +1,3 @@
-<main class="c-main p-main">
-  <div class="p-main__inner p-archive__inner">
-    <ul class="p-staff__list">
-      <?php
-      $args = array(
-        'post_type' => $post_type,
-        'posts_per_page' => -1, // 取得する投稿数を設定（−1は無制限）
-        'order' => 'DESC', //並び順を指定
-        'orderby' => 'date',  // 並び変える項目を設定
-      );
-      get_template_part('parts/project/p-staff-card', null, $args);
-      ?>
-    </ul>
-  </div>
-</main>
+<ul class="p-staff__list">
+  <?php get_template_part('parts/project/p-staff-card', null, $args); ?>
+</ul>
