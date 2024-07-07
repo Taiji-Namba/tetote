c3313a1c9ddbf931e81647cedbdd25897bcb6a5b
Taiji-Namba <namba9499@gmail.com>
2024-07-07 21:49:49 +0900

    p-eyecatch.phpの再利用性向上

--- app/public/wp-content/themes/tetote/archive.php
+++ app/public/wp-content/themes/tetote/archive.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
-<?php get_template_part('parts/common/p-eyecatch'); ?>
+<?php
+$args = ['class' => 'blog', 'title' => 'BLOG', 'subtitle' => '採用ブログ', 'text' => '採用情報やイベント情報などをご紹介します。'];
+get_template_part('parts/common/p-eyecatch', null, $args); ?>
 
 <?php get_template_part('parts/common/p-breadcrumb'); ?>
 
