66bfbcfd046520cae0dfcd7baf182f35784f4c55
Taiji-Namba <namba9499@gmail.com>
2024-07-07 09:46:17 +0900

    トップFVのNEWSの内容を、最新のブログ記事のタイトルに

--- app/public/wp-content/themes/tetote/parts/project/p-top-fv.php
+++ app/public/wp-content/themes/tetote/parts/project/p-top-fv.php
@@ -8,9 +8,25 @@
   <a href="<?php echo esc_url(home_url() . '/blog/') ?>" class="p-news p-fv__news">
     <h3 class="p-news__heading">NEWS</h3>
     <div class="p-news__text">
-      <p class="p-news__title">2024年度働きやすさランキング一位を取得しました！！</p>
+      <p class="p-news__title">
+        <?php
+        $args = array(
+          'post_type' => 'post',
+          'posts_per_page' => 1,
+        );
+        $the_query = new WP_Query($args);
+        ?>
+        <?php if ($the_query->have_posts()) : ?>
+          <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+            <?php the_title() ?>
+          <?php endwhile; ?>
+        <?php else : ?>
+          まだ採用ブログ記事はありません。
+        <?php endif;
+        wp_reset_postdata(); ?>
+      </p>
       <p class="p-news__cta">
-        VIEW MORE
+        <span>VIEW MORE</span>
         <img class="p-news__arrow" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/arrow.svg'); ?>" alt="">
       </p>
     </div>
