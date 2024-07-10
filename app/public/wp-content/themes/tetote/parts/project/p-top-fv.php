1595177e7183631be933875826550f1ae5f39ce0
Taiji-Namba <namba9499@gmail.com>
2024-07-10 21:50:01 +0900

    NEWSのリンクを記事詳細ページに修正

--- app/public/wp-content/themes/tetote/parts/project/p-top-fv.php
+++ app/public/wp-content/themes/tetote/parts/project/p-top-fv.php
@@ -5,30 +5,41 @@
   </div>
   <h2 class="p-fv__heading">BECOME A <br> CHALLENGER.</h2>
   <p class="p-fv__text">君の挑戦が、意思が、未来を変える</p>
-  <a href="<?php echo esc_url(home_url() . '/blog/') ?>" class="p-news p-fv__news">
-    <h3 class="p-news__heading">NEWS</h3>
-    <div class="p-news__text">
-      <p class="p-news__title">
-        <?php
-        $args = array(
-          'post_type' => 'post',
-          'posts_per_page' => 1,
-        );
-        $the_query = new WP_Query($args);
-        ?>
-        <?php if ($the_query->have_posts()) : ?>
-          <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+  <?php
+  $args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 1,
+  );
+  $the_query = new WP_Query($args);
+  ?>
+  <?php if ($the_query->have_posts()) : ?>
+    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+      <a href="<?php the_permalink(); ?>" class="p-news p-fv__news">
+        <h3 class="p-news__heading">NEWS</h3>
+        <div class="p-news__text">
+          <p class="p-news__title">
             <?php the_title() ?>
-          <?php endwhile; ?>
-        <?php else : ?>
+          </p>
+          <p class="p-news__cta">
+            <span class="p-news__cta-text">VIEW MORE</span>
+            <img class="p-news__arrow" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/arrow.svg'); ?>" alt="">
+          </p>
+        </div>
+      </a>
+    <?php endwhile; ?>
+  <?php else : ?>
+    <a href="" class="p-news p-fv__news">
+      <h3 class="p-news__heading">NEWS</h3>
+      <div class="p-news__text">
+        <p class="p-news__title">
           まだ採用ブログ記事はありません。
-        <?php endif;
-        wp_reset_postdata(); ?>
-      </p>
-      <p class="p-news__cta">
-        <span class="p-news__cta-text">VIEW MORE</span>
-        <img class="p-news__arrow" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/arrow.svg'); ?>" alt="">
-      </p>
-    </div>
-  </a>
+        </p>
+        <p class="p-news__cta">
+          <span class="p-news__cta-text">VIEW MORE</span>
+          <img class="p-news__arrow" src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/arrow.svg'); ?>" alt="">
+        </p>
+      </div>
+    </a>
+  <?php endif;
+  wp_reset_postdata(); ?>
 </section>
