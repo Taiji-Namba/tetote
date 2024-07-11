eabe5f872558e820a8406d77b766360ffab5209d
Taiji-Namba <namba9499@gmail.com>
2024-07-11 14:06:07 +0900

    電話番号やメールアドレスの自動リンク化を無効化

--- app/public/wp-content/themes/tetote/header.php
+++ app/public/wp-content/themes/tetote/header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name=”format-detection” content=”email=no,telephone=no,address=no”>
 
   <?php wp_head(); ?>
 </head>
