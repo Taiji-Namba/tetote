4035020bd9a6c61cce7a4771116732fc7a7de1b4
Taiji-Namba <namba9499@gmail.com>
2024-07-11 14:39:44 +0900

    IE対応のmetaタグとtelephone=noの重複を削除

--- app/public/wp-content/themes/tetote/header.php
+++ app/public/wp-content/themes/tetote/header.php
@@ -4,8 +4,6 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="format-detection" content="telephone=no">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name=”format-detection” content=”email=no,telephone=no,address=no”>
 
   <?php wp_head(); ?>
