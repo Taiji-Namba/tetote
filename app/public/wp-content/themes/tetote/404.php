da2f73d216a2ecd7e7913795e86e769ff88f90f1
Taiji-Namba <namba9499@gmail.com>
2024-07-18 09:20:47 +0900

    404ページのスタイリング

--- app/public/wp-content/themes/tetote/404.php
+++ app/public/wp-content/themes/tetote/404.php
@@ -2,9 +2,9 @@
 <main class="l-main">
   <div class="p-404">
     <div class="p-404__inner l-inner">
-      <h2 class ="p-404__title c-common-title">お探しのページは見つかりませんでした。</h2>
+      <h2 class="p-404__title c-common-title">お探しのページは見つかりませんでした。</h2>
       <div class="p-404__button">
-        <a class="c-button" href="<?php echo esc_url(home_url($page)); ?>">TOPページへ</a>
+        <a class="c-button p-button" href="<?php echo esc_url(home_url($page)); ?>">TOPページへ</a>
       </div>
     </div>
   </div>
