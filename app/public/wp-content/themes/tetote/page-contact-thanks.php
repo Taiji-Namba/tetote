829420dfe6cb9c567db94d9c5711cf8ffb843d9a
Taiji-Namba <namba9499@gmail.com>
2024-07-24 10:05:49 +0900

    エントリーサンクスページパーフェクトピクセル

--- app/public/wp-content/themes/tetote/page-contact-thanks.php
+++ app/public/wp-content/themes/tetote/page-contact-thanks.php
@@ -8,15 +8,17 @@ Template Name: ENTRY-THANKS
 
 <?php get_template_part('parts/common/p-breadcrumb'); ?>
 
-<main class="l-main">
-  <section class="p-contact">
-    <div class="p-contact__wrapper">
-      <div class="p-contact__inner p-contact__inner--pb95">
-        <h1 class="p-contact__title p-contact__title--thanks u-justify-content--center">送信ありがとうございました。</h1>
-        <p class="p-contact__description u-justify-content--center">3営業日以内に人事担当者より連絡させていただきます。</p>
+<main class="c-main p-main">
+  <div class="c-main__inner p-main__inner">
+    <section class="p-contact">
+      <div class="p-contact__wrapper">
+        <div class="p-contact__inner p-contact__inner--pb95">
+          <h1 class="p-contact__title p-contact__title--thanks u-justify-content--center">送信ありがとうございました。</h1>
+          <p class="p-contact__description u-justify-content--center">3営業日以内に人事担当者より連絡させていただきます。</p>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </main>
 
 <?php get_footer(); ?>
